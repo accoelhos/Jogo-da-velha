fcb8101d5d3021efd860bffa8901c00eba443cbb
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-19 16:30:47 -0300

    update

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -2,6 +2,7 @@ import Jogo from "./components/jogo/Jogo";
 function App() {
   return (
     <div className="App">
+      
       <Jogo/>
     </div>
   );
--- jogo-da-velha/src/components/jogo/Jogo.jsx
+++ jogo-da-velha/src/components/jogo/Jogo.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Jogo.css";
 
-function Jogo(){
+import circle from "./../assets/anel-circular.png";
+import x from "./../assets/x.png";
+
+function Jogo() {
     return (
         <>
-        
-        
+        <div className="container flex flex-col items-center pt-4 min-h-screen">
+            <h2 className="title text-white">Jogo da velha</h2>
+            <hr className="divider" />
+        </div>
         </>
     )
 }
