f8f91f3ff52db72b8cc60ae9e5d35bd6793ab48e
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-19 00:23:45 -0300

    create proj

---
+++ jogo-da-velha/src/App.js
@@ -0,0 +1,10 @@
+import Jogo from "./components/jogo/Jogo";
+function App() {
+  return (
+    <div className="App">
+      <Jogo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ jogo-da-velha/src/components/jogo/Jogo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Jogo.css";
+
+function Jogo(){
+    return (
+        <>
+        
+        
+        </>
+    )
+}
+export default Jogo;
